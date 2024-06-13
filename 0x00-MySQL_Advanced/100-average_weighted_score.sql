0bcf7ebc828685e341368496d72473ede79babee
obusandy <sandraobunga29@gmail.com>
2024-06-13 23:46:47 +0300

    my changes

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -2,18 +2,18 @@
 -- drop procedure if it exists
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    user_id INT
+)
 BEGIN
--- Declare a variable to store the weighted
-    DECLARE weit_avg_score FLOAT;
-    SET weit_avg_score =
-                        (SELECT SUM(score * weight) / SUM(weight)
-                        FROM users AS Usr
-                        JOIN corrections as Crrt ON Usr.id=Crrt.user_id
-                        JOIN projects AS Prj ON Crrt.project_id=Prj.id
-                        WHERE Usr.id=user_id)
-    UPDATE users
-    SET average_score = weit_avg_score
-    WHERE id=user_id;
-END $$
+-- declare variable
+    DECLARE w_avg_score FLOAT;
+    SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight)
+                        FROM users AS U
+                        JOIN corrections as C ON U.id=C.user_id
+                        JOIN projects AS P ON C.project_id=P.id
+                        WHERE U.id=user_id);
+    UPDATE users SET average_score = w_avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
